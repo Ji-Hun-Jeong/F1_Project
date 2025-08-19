f3034224d219203cd95e7f6a9ded9010cceec0bc
JiHun <wjdwlgns8096@naver.com>
2025-08-19 09:19:07 +0000

    Save

--- Users/project/src/server/main.py
+++ Users/project/src/server/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from response_data import router  # routes.py에서 router 가져오기
 
 app = FastAPI()
 
+# 🔧 CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 개발 중엔 *로 열어두고, 배포 시엔 도메인 제한
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 라우터 등록
+app.include_router(router)
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello, Azure VM with FastAPI!"}
+def gd():
+    return {"message": "Hi"}
